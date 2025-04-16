79dce07e7dcd5849a52ee5274b4738a5b89351d5
Michal <kafej666@gmail.com>
2025-04-16 11:34:59 +0200

    Add files via upload

---
+++ network_monitor.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# This need to be yours DB info
+DB_HOST="192.168.50.72"
+DB_USER="external"
+DB_PASSWORD="Qwerty123"
+DB_NAME="vault"
+
+# Network Interface Names
+INTRA_IFACE="eth0"
+VAULT_IFACE="eth1"
+
+# Function to check database and switch networks
+switch_networks_status() {
+  # Check if any rows have "no" in the "done" column
+  HAS_YES=$(mysql -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" -D "$DB_NAME" -s -N -e "SELECT done FROM requests ORDER BY id DESC LIMIT 1;" | grep "yes")
+
+  if [ -n "$HAS_YES" ]; then
+    # Down Intranet, Up Vault
+    ip link set "$INTRA_IFACE" up
+    ip link set "$VAULT_IFACE" down
+    echo "Switched to Intranet network."
+    bash /home/kafej/Pulpit/network_switch.sh
+    exit 0
+  fi
+}
+
+# Run the function
+switch_networks_status
+
+# Loop indefinitely, checking network status every 5 seconds
+while true; do
+  switch_networks_status
+  sleep 5
+done
